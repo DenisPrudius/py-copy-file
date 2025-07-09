fda3af6b5599b4f558e7a841798189e4679edb49
benfict <benfict@gmail.com>
2025-07-09 23:51:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.strip().split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid command format")
+        return
+
+    cp, source, target = parts
+
+    if source == target:
+        return
+
+    try:
+        with open(source, "r") as file_in, open(target, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(f"File '{source}' not found.")
