7cee6958dbbc50bf7c0eba239d33a608c5b15587
benfict <benfict@gmail.com>
2025-07-10 23:10:42 +0300

    Fix Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 def copy_file(command: str) -> None:
     parts = command.strip().split()
 
-    if len(parts) != 3 or parts[0] != "cp":
-        print("Invalid command format")
+    if len(parts) != 3:
+        print("Error: "
+              "command must have exactly three parts:"
+              " 'cp source target'")
+        return
+
+    if parts[0] != "cp":
+        print("Error: command must start with 'cp'")
         return
 
     cp, source, target = parts
@@ -12,6 +18,7 @@ def copy_file(command: str) -> None:
 
     try:
         with open(source, "r") as file_in, open(target, "w") as file_out:
-            file_out.write(file_in.read())
+            for line in file_in:
+                file_out.write(line)
     except FileNotFoundError:
         print(f"File '{source}' not found.")
